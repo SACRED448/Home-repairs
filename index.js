4432ab290e3d8ea63a74f1c26d9b0222de0b2116
Anastasia <zharkovanastia644@gmail.com>
2023-12-30 18:29:14 +0700

    Полная стилизация header. Начало написания стилей для блока "Our works"

--- index.js
+++
@@ -1,7 +0,0 @@
-// window.addEventListener('scroll', function() {
-//   if (pageYOffset > 50) {
-//     document.querySelector('.header').classList.add('active')
-//   } else {
-//     document.querySelector('.header').classList.remove('active')
-//   }
-// })
