670fe1a4b88de20ec323c729ef8236f5d9247287
Anastasia <zharkovanastia644@gmail.com>
2024-02-07 20:07:38 +0700

    Вёрстка блока "Наши работы", добавление функционала. Без изменений.

--- js/main.js
+++ js/main.js
@@ -15,3 +15,31 @@ $(function() {
 
   $('.before-and-after').beforeAfter();
 });
+
+
+// Рабочий код, меняет картинки по нажатию на кнопку,
+// сложно применить для изображений слайдеров.
+// const arrayOfImage = [
+//   'img/our-works-1.png',
+//   'img/our-works-2.png',
+//   'img/our-works-3.png',
+//   'img/our-works-4.png'
+// ];
+
+// const img = document.querySelector('SELECTOR');
+// const arrowInRight = document.querySelector('.our-works-purple-arrow-in-right');
+
+
+// let numImage = 0;
+
+// img.src = arrayOfImage[numImage];
+
+// arrowInRight.addEventListener('click', () => {
+//   numImage++;
+//   if (numImage >= arrayOfImage.length) {
+//     numImage = 0;
+//   }
+//   console.log(numImage >= arrayOfImage.length);
+//   console.log(numImage);
+//   img.src = arrayOfImage[numImage];
+// });
